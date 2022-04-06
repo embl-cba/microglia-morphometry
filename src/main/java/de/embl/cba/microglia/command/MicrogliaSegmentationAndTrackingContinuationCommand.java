8cb95259b83c525bb9b57b13989b126ee34a682c
Christian Tischer <christian.tischer@embl.de>
2022-04-06 10:08:51 +0200

    Add documentation image and pptx

--- src/main/java/de/embl/cba/microglia/command/MicrogliaSegmentationAndTrackingContinuationCommand.java
+++ src/main/java/de/embl/cba/microglia/command/MicrogliaSegmentationAndTrackingContinuationCommand.java
@@ -1,6 +1,5 @@
 package de.embl.cba.microglia.command;
 
-import de.embl.cba.microglia.MicrogliaSettings;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import org.scijava.command.Command;
@@ -8,7 +7,6 @@ import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 import java.io.File;
-
 
 @Plugin(type = Command.class, menuPath = "Plugins>Microglia>Continue Microglia Segmentation And Tracking" )
 public class MicrogliaSegmentationAndTrackingContinuationCommand< T extends RealType<T> & NativeType< T > > extends MicrogliaSegmentationAndTrackingCommand
