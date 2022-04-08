57618cce9529864f628cd73ce09e28d9c5f74a87
Christian Tischer <christian.tischer@embl.de>
2022-04-08 16:48:29 +0200

    Add segmentation ij-macro scripts

---
+++ src/test/java/example/RunImageJ.java
@@ -0,0 +1,12 @@
+package example;
+
+import net.imagej.ImageJ;
+
+public class RunImageJ
+{
+	public static void main( String[] args )
+	{
+		final ImageJ ij = new ImageJ();
+		ij.ui().showUI();
+	}
+}
