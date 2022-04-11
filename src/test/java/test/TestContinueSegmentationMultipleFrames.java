d01824a4146518c2dd0db19bdcdbf7640b64ed31
Christian Tischer <christian.tischer@embl.de>
2022-04-11 15:06:44 +0200

    Correct image calibration

--- src/test/java/test/TestContinueSegmentationMultipleFrames.java
+++ src/test/java/test/TestContinueSegmentationMultipleFrames.java
@@ -14,8 +14,8 @@ public class TestContinueSegmentationMultipleFrames
 		final ImageJ ij = new ImageJ();
 		final MicrogliaSegmentationAndTrackingContinuationCommand command = new MicrogliaSegmentationAndTrackingContinuationCommand();
 		command.opService = ij.op();
-		command.intensityFile = new File("https://github.com/embl-cba/microglia-morphometry/raw/main/src/test/resources/data/MAX_pg6-3CF1_20--t1-5.tif");
-		command.segmentationFile =  new File("https://github.com/embl-cba/microglia-morphometry/raw/main/src/test/resources/data/MAX_pg6-3CF1_20--t1-3-labelMasks.tif");
+		command.intensityFile = new File("https://github.com/embl-cba/microglia-morphometry/raw/main/src/test/resources/data/MAX_pg6-3CF1_20--t1-3.tif");
+		command.segmentationFile = new File("https://github.com/embl-cba/microglia-morphometry/raw/main/src/test/resources/data/MAX_pg6-3CF1_20--t1-3-labelMasks.tif");
 		command.outputDirectory = new File( "src/test/resources/data" );
 		command.relativeIntensityThreshold = 1.5;
 		command.run();
